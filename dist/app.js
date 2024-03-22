434ed69c5d71218c768e516fa5550b0b8fe7e523
mramzannasir <mramzannasir555@gmail.com>
2024-03-22 16:33:02 +0500

    server file add

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,9 @@
 import express from "express";
-import { connectDB } from "./utils/connectDB.js";
 import userRoutes from "./routes/user.js";
-const app = express();
-connectDB();
+export const app = express();
 app.use(express.json());
 app.use("/api/v1/user", userRoutes);
+// 🏘️ Home route
 app.get("/", (req, res) => {
     res.send("Welcome to e-commerce backend");
-});
-app.listen(4040, () => {
-    console.log("Backend is now working on port 4040");
 });
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { app } from "./app.js";
+import { connectDB } from "./utils/connectDB.js";
+
+
+connectDB();
+
+
+
+app.listen(4040, () => {
+  console.log("Backend is now working on port 4040");
+});
