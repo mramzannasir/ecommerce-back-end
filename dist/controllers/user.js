5f0a172de98e73a5dd94c3734f812503e66274af
mramzannasir <mramzannasir555@gmail.com>
2024-03-22 13:00:02 +0500

    console removed

--- dist/controllers/user.js
+++ dist/controllers/user.js
@@ -2,7 +2,6 @@ import { User } from "../models/user.js";
 export const registerUser = async (req, res, next) => {
     try {
         const { name, email, password, photo, _id, dob } = req.body;
-        console.log(name, email, password, photo, dob);
         const user = await User.create({
             name,
             email,
@@ -14,10 +13,11 @@ export const registerUser = async (req, res, next) => {
         res.status(201).json({
             success: true,
             status: 201,
-            message: `User registration successful. Welcome, ${user.name}!`,
+            message: `Welcome, ${user.name}!`,
         });
     }
     catch (error) {
+        console.log(error);
         res.status(400).json({
             success: false,
             status: 400,
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -9,7 +9,6 @@ export const registerUser = async (
 ) => {
   try {
     const { name, email, password, photo, _id, dob } = req.body;
-    console.log(name, email, password, photo, dob);
     const user = await User.create({
       name,
       email,
@@ -21,9 +20,10 @@ export const registerUser = async (
     res.status(201).json({
       success: true,
       status: 201,
-      message: `User registration successful. Welcome, ${user.name}!`,
+      message: `Welcome, ${user.name}!`,
     });
   } catch (error) {
+    console.log(error);
     res.status(400).json({
       success: false,
       status: 400,
