695060a691b26a47241f2a656225907f3605cf96
mramzannasir <mramzannasir555@gmail.com>
2024-11-09 22:41:58 +0500

    testing updates

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,5 @@ import { connectDB } from "./utils/connectDB.js";
 connectDB();
 
 app.listen(4040, () => {
-  console.log("Backend is now working on port 4040");
+  console.log("Backend is now working on port 4040 congratulation!");
 });
