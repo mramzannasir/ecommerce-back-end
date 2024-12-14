80e68c5b30503590317ab8f59c286f311ad25bee
Muhammad Ramzan Nasir <87058400+mramzannasir@users.noreply.github.com>
2024-12-14 14:52:54 +0500

    dist folder deleted

--- dist/routes/user.js
+++
@@ -1,8 +0,0 @@
-import { adminOnly } from "../middlewares/admin.js";
-import { deleteUser, getAllUser, getUser, registerUser, } from "./../controllers/user.js";
-import express from "express";
-const app = express.Router();
-app.post("/register", registerUser);
-app.get("/all/:id", adminOnly, getAllUser);
-app.route("/:id").get(getUser).delete(adminOnly, deleteUser);
-export default app;
